0b118fd5b9a2eb3060a604e5ad2c06fd5e49bcaf
Junaid Ali <junaid.hyly@gmail.com>
2023-07-18 00:39:35 -0700

    Added jenkins code

---
+++ terraform/jenkins-common/instance.tf
@@ -0,0 +1,62 @@
+resource "google_service_account" "jenkins_sa" {
+  account_id   = "jenkins-sa"
+  display_name = "Jenkins Service Account"
+  project = local.project
+}
+resource "google_compute_instance" "jenkins" {
+  name         = local.name
+  machine_type = local.machine_type
+  zone         = local.zone
+
+  tags                      = ["ssh","publicaccess"]
+  allow_stopping_for_update = local.allow_stopping_for_update
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        owner       = "terraform"
+        managed_by  = "junaid-ali"
+        environment = local.environment
+      }
+
+      size = local.disk_size
+    }
+  }
+
+  metadata_startup_script = "${file("install-jenkins.sh")}"
+
+  network_interface {
+    network    = data.google_compute_network.hyly-network.name
+    subnetwork = data.google_compute_network.hyly-network.subnetworks_self_links[0] # public subnet
+
+    access_config {
+      // Ephemeral/Static public IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = local.allowed_ssh_pub_keys
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.jenkins_sa.email
+    scopes = ["cloud-platform"]
+  }
+  depends_on = [ google_service_account.jenkins_sa ]
+}
+
+resource "google_compute_firewall" "public_access" {
+  name      = "public-access-rule"
+  network   = data.google_compute_network.hyly-network.name
+  priority  = 1000
+  direction = "INGRESS"
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["publicaccess"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+}
---
+++ terraform/jenkins-common/locals.tf
@@ -0,0 +1,28 @@
+locals {
+  project     = "gds-prototype-20190629" # project name = GoogleDataStudio
+  region      = "us-east1"
+  environment = "staging-dev"
+
+  # network
+  vpc_network_name = "hyly-ml"
+
+  # staging-dev environment state bucket
+  jenkins_state_bucket_name = "jenkins-gcp-state"
+  jenkins_bucket_location   = "us-east1"
+
+  # compute engine
+  name                      = "jenkins"
+  machine_type              = "n2-standard-2"
+  zone                      = "us-east1-b"
+  disk_size                 = 120
+  allow_stopping_for_update = true
+
+  allowed_ips_for_ssh = [
+    "122.129.66.210/32", # Junaid's IP
+    "58.27.193.244/32"   # Junaid's IP
+  ]
+  allowed_ssh_pub_keys = <<EOT
+                ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVpBmAHtXTF5o3QPImmJgHYL0gKqi/qZH0em/Fj9cAzva4lC/bvm4CHJxSfHqtZHF6pcrCm3WBK5EOZw3jc6ZR3grfwwm0OTjRWHVSVFCv3XHgfVXNSsVsfi4fYhOv+RIsXYvH9middJYJmrf2riHghlujQHwPuA2Y7y0KqD+nv4M9rTvk2g1nieg7Yx61iFKm5ygSBqOlJd6W7TtdocBZ0H65U/J2XyZkYJpLXvX/pjRd9HfbnheTKtyL0uHNNUSyNGaH/s43JdkUX6O4aw1T86LuOgegljHBjp0/j7WihEtAZvENT2Ar0nW/Csak8Diin6pexBrvQJIsAo7NyP4T ja-keypair
+                ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCa8HfqHd3BhoHtXSasgbQ9/HnpEByNknicekS4LTfvZA5GKgjZm8XA0v+tyy1k5LBHQ7umVG0Dm8zuVGDJH5nC/N1zXi419csaIAvdDaEBmZQ0y4PxnW4c1Rw8oxwg43drvffGGjtGTFv5JfiaZVT1WUnVUqCnsTfbfUqsJ9eo9OM8+kQhtOIMr2A5IgFG5YmmlL5e6bL2TeHJPC4P/dE5M4KbkRQASqA4mAEojno562JR5EUa0QC1Gc3Rvrv5aREaVFZokP7r5nkRUU7fyrQ/FYwCFUr0622qKS/EIKszqhlNVnWBvA/KuivRscPZPBJY2kfzif438kOBnzVDyHuH tn-keypair
+  EOT
+}
