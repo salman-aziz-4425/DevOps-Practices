00ff163bd6aa9115acf9c16dbdbb8792bf63221e
Junaid Ali <junaid.hyly@gmail.com>
2023-07-25 05:36:31 -0700

    Fixed volumes permission denied issue - updated script

--- docker/environments/staging/deployment.sh
+++ docker/environments/staging/deployment.sh
@@ -2,11 +2,11 @@
 
 # One time manual deployment script
 
+# Start Docker Compose services
+docker compose up -d
+
 # Change permissions of the host directory
 sudo chmod -R 755 ~/.volumes
 
 # Change ownership of the host directory to a specific user and group
 sudo chown -R ubuntu:docker ~/.volumes
-
-# Start Docker Compose services
-docker compose up -d
