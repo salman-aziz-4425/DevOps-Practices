db033d855d514abecf125524e83a4d21217553d0
Junaid Ali <junaid.hyly@gmail.com>
2023-11-16 20:13:23 +0500

    Add Variables description and Add IP

--- rails7_infra/terraform/environments/qa_staging/locals.tf
+++ rails7_infra/terraform/environments/qa_staging/locals.tf
@@ -7,13 +7,14 @@ locals {
 
   allowed_ips_for_ssh = [
     "14.237.225.29/32", # Tin IP
-    "122.129.66.106/32" # Junaid IP
+    "122.129.66.106/32", # Junaid IP
+    "58.27.193.244/32"   # Junaid IP
   ]
 
   vpc_azs = ["us-east-2a", "us-east-2b"]
   vpc_public_subnets = [
     "10.0.0.0/24",
-    "10.0.1.0/24"
+    "10.0.1.0/24",
   ]
   sys_stage_vpc_id    = "vpc-a3d9d7c5"
   sys_stage_subnet_id = "subnet-040b206a1e260b0d2"
--- rails7_infra/terraform/modules/ec2/variables.tf
+++ rails7_infra/terraform/modules/ec2/variables.tf
@@ -1,39 +1,41 @@
 variable "project" {
   type        = string
+  description = "Project name"
 }
 variable "region" {
   type        = string
+  default = "region name"
 }
 variable "profile" {
   type        = string
+  default = "Profile"
 }
 variable "environment" {
   type        = string
+  description = "project environement"
 }
 variable "instance_type_qa_stage" {
   type        = string
+  description = "Instance type"
 }
 variable "allowed_ips_for_ssh" {
   type        = list
+  description = "allows IP for ssh"
 }
 variable "vpc_azs" {
   type        = list
+  description = "availability zones"
 }
 variable "vpc_public_subnets" {
   type        = list
+  description = "public subnets"
 }
 variable "sys_stage_vpc_id" {
   type        = string
+  description = "VPC"
 }
 variable "sys_stage_subnet_id" {
   type        = string
+  description = "subnet"
 }
 
-# variable "tags" {
-#   type = list(object({
-#     Project           = string
-#     Environment             = string
-#     Owner         = string
-#     ManagedBy = string 
-#   }))
-# }
